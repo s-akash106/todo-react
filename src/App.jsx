93296b030392a5153f51ddbf7adcbfe504ab1c29
s-akash106 <akashsuryavanshi8900@gmail.com>
2024-11-06 22:23:35 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <center className="todo-container">
+        <h1>TODO APP</h1>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-sm-5" >
+            <input type="data"></input>
+            </div>
+            
+          <div className="col-sm-5">
+            <input type="date"></input>
+            </div>
+            
+            <div className="col-sm-2"><button type="button" className="btn btn-success">Success</button></div>
+            
+          </div>
+          
+        </div>
+
+
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-sm-5" >
+            <input type="data"></input>
+            </div>
+            
+          <div className="col-sm-5">
+            <input type="date"></input>
+            </div>
+            
+            <div className="col-sm-2"><button type="button" className="btn btn-success">Success</button></div>
+            
+          </div>
+          
+        </div>
+
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-sm-5" >
+            <input type="data"></input>
+            </div>
+            
+          <div className="col-sm-5">
+            <input type="date"></input>
+            </div>
+            
+            <div className="col-sm-2"><button type="button" className="btn btn-success">Success</button></div>
+            
+          </div>
+          
+        </div>
+        
+
+    </center>
+    </>
+  )
+}
+
+export default App
